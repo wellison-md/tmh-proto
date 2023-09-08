232d1b648bf4928f6bce398789431a833f969a02
Wellison M. Dias <wellisonmdias@gmail.com>
2023-09-08 11:54:45 -0300

    :zap: feat: create cardlist-iterator

---
+++ src/components/cardList/index.jsx
@@ -0,0 +1,24 @@
+import PropTypes from 'prop-types';
+import PetCard from '../card';
+
+export default function CardList(props) {
+  const { petList } = props;
+  return (
+    <>
+      {
+        petList.length === 0
+          ? 'loading...'
+          : petList.map((item) => <PetCard key={ item.id } { ...item } />)
+      }
+    </>
+  );
+}
+
+CardList.propTypes = {
+  petList: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    nick: PropTypes.string,
+    image: PropTypes.string,
+    rescue_date: PropTypes.string,
+  }))
+}
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,15 +1,15 @@
 import { useEffect, useState } from "react";
 import { getFake } from "../../utils/fkdb/getFkData";
 import NavBar from "../../components/navBar";
+import CardList from "../../components/cardList";
 
 export default function Home() {
   const [data, setData] = useState([]);
 
   useEffect(() => {
     (async () => {
-      const x = await getFake('pets');
-      console.log(x)
-      setData(x)
+      const petList = await getFake('pets');
+      setData(petList)
     })()
   }, []);
 
@@ -17,14 +17,7 @@ export default function Home() {
     <>
       <h1>Home</h1>
       <NavBar />
-      {
-        data.length === 0
-          ? 'loading...'
-          : data.map((item) => (
-            <ul key={ item.id }>{ item.nick }
-              <img src={ item.image } alt={ item.nick } />
-            </ul>))
-      }
+      <CardList petList={ data } />
     </>
   );
 }
