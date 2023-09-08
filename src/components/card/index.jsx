84e50c3fc26a2ce4b7f82b8a2dfee8734b6f487e
Wellison M. Dias <wellisonmdias@gmail.com>
2023-09-08 14:47:23 -0300

    :zap: feat: add links to card-component

--- src/components/card/index.jsx
+++ src/components/card/index.jsx
@@ -1,21 +1,26 @@
 import PropTypes from 'prop-types';
 import { calculatePetcoins } from '../../utils/petCoins';
+import { formatDateToView } from '../../utils/handleDate';
+import { Link } from 'react-router-dom';
 
 export default function PetCard(props) {
-  const { image, nick, rescue_date, } = props;
+  const { id, image, nick, rescue_date, } = props;
   return (
     <>
-      <img src={ image } alt={ nick } />
-      <h3>{ nick }</h3>
-      <span>
-        <p>Resgatado em: { rescue_date }</p>
-        <p>PetCoins: { calculatePetcoins(rescue_date) }</p>
-      </span>
+      <Link to={ `/pet-profile/${id}` } >
+        <img src={image} alt={nick} />
+        <h3>{nick}</h3>
+        <span>
+          <p>Resgatado em: {formatDateToView(rescue_date)}</p>
+          <p>PetCoins: {calculatePetcoins(rescue_date)}</p>
+        </span>
+      </Link>
     </>
   );
 }
 
 PetCard.propTypes = {
+  id: PropTypes.number.isRequired,
   image: PropTypes.string,
   nick: PropTypes.string,
   rescue_date: PropTypes.string,
