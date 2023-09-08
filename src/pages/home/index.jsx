15409364820815456501ff3daa82a509563f8a9b
Wellison M. Dias <wellisonmdias@gmail.com>
2023-09-07 22:11:59 -0300

    :recycle: refact: update values on pet-data

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,12 +1,15 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { getFake } from "../../utils/fkdb/getFkData";
 import NavBar from "../../components/navBar";
 
 export default function Home() {
+  const [data, setData] = useState([]);
+
   useEffect(() => {
     (async () => {
       const x = await getFake('pets');
       console.log(x)
+      setData(x)
     })()
   }, []);
 
@@ -14,6 +17,14 @@ export default function Home() {
     <>
       <h1>Home</h1>
       <NavBar />
+      {
+        data.length === 0
+          ? 'loading...'
+          : data.map((item) => (
+            <ul key={ item.id }>{ item.nick }
+              <img src={ item.image } alt={ item.nick } />
+            </ul>))
+      }
     </>
   );
 }
--- src/utils/fkdb/fkPets.js
+++ src/utils/fkdb/fkPets.js
@@ -1,14 +1,18 @@
+// import img1 from '../../assets/carousel/@480p__bird2.jpg'
+// import { CAROUSEL_IMG_1, CAROUSEL_IMG_2 } from '../configs'
+
 export const pets = [
   {
     id: 1,
     nick: 'Alfred',
     category: 'cat',
     gender: 'male',
-    age: '',
+    age: '0.5yo',
     rescue_date: '2023-05-07',
-    image: 'https://cdn.pixabay.com/photo/2014/11/30/14/11/cat-551554_640.jpg',
+    image: 'src/assets/carousel/@480p__Alfredo.jpg',
     galery: [
       'https://cdn.pixabay.com/photo/2016/03/28/10/05/kitten-1285341_640.jpg',
+      'https://cdn.pixabay.com/photo/2014/11/30/14/11/cat-551554_640.jpg',
       'https://cdn.pixabay.com/photo/2015/06/07/19/42/animal-800760_640.jpg',
       'https://cdn.pixabay.com/photo/2016/11/29/01/10/kitten-1866475_640.jpg',
       'https://cdn.pixabay.com/photo/2019/10/17/14/52/kitten-4557097_640.jpg',
@@ -20,20 +24,79 @@ export const pets = [
   },
   {
     id: 2,
-    nick: 'Alfred',
-    category: 'cat',
+    nick: 'Piu piu',
+    category: 'bird',
     gender: 'male',
-    age: '0.6yo',
+    age: 'unknown',
     rescue_date: '2022-01-17',
-    image: 'https://cdn.pixabay.com/photo/2014/11/30/14/11/cat-551554_640.jpg',
+    image: 'src/assets/carousel/@480p__bird2.jpg',
     galery: [
-      'https://cdn.pixabay.com/photo/2016/03/28/10/05/kitten-1285341_640.jpg',
-      'https://cdn.pixabay.com/photo/2015/06/07/19/42/animal-800760_640.jpg',
-      'https://cdn.pixabay.com/photo/2016/11/29/01/10/kitten-1866475_640.jpg',
-      'https://cdn.pixabay.com/photo/2019/10/17/14/52/kitten-4557097_640.jpg',
+      'https://images.pexels.com/photos/4430026/pexels-photo-4430026.jpeg?auto=compress&cs=tinysrgb&w=600',
+      'https://images.pexels.com/photos/6398824/pexels-photo-6398824.jpeg?auto=compress&cs=tinysrgb&w=600',
+      'https://images.pexels.com/photos/6477610/pexels-photo-6477610.jpeg?auto=compress&cs=tinysrgb&w=600',
     ],
     vaccins: [
       'vaccin1',
     ]
+  },
+  {
+    id: 3,
+    nick: 'Batman',
+    category: 'dog',
+    gender: 'female',
+    age: '2.5yo',
+    rescue_date: '2022-10-22',
+    image: 'src/assets/carousel/@480p__batman.jpg',
+    galery: [
+      'https://images.pexels.com/photos/10201413/pexels-photo-10201413.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load',
+      'https://images.pexels.com/photos/260600/pexels-photo-260600.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load',
+      'https://images.pexels.com/photos/16652416/pexels-photo-16652416/free-photo-of-animal-bicho-fofo-bonitinho.jpeg?auto=compress&cs=tinysrgb&w=600',
+    ],
+    vaccins: [
+      'vaccin1',
+    ]
+  },
+  {
+    id: 4,
+    nick: 'Fisher',
+    category: 'fish',
+    gender: 'male',
+    age: 'unknown',
+    rescue_date: '2023-05-22',
+    image: 'src/assets/carousel/@480p_fisher.jpg',
+    galery: [
+      'https://images.pexels.com/photos/7186210/pexels-photo-7186210.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load',
+    ],
+    vaccins: []
+  },
+  {
+    id: 5,
+    nick: 'Mr speed',
+    category: 'wild',
+    gender: 'male',
+    age: '3yo',
+    rescue_date: '2020-12-20',
+    image: 'src/assets/carousel/@480p_mr-speedy.jpg',
+    galery: [
+      'https://images.pexels.com/photos/914794/pexels-photo-914794.jpeg?auto=compress&cs=tinysrgb&w=600',
+      'https://images.pexels.com/photos/2253789/pexels-photo-2253789.jpeg?auto=compress&cs=tinysrgb&w=600',
+      'https://images.pexels.com/photos/3487177/pexels-photo-3487177.jpeg?auto=compress&cs=tinysrgb&w=600',
+    ],
+    vaccins: []
+  },
+  {
+    id: 6,
+    nick: 'Larinha',
+    category: 'bird',
+    gender: 'female',
+    age: '1.2yo',
+    rescue_date: '2022-04-03',
+    image: 'src/assets/carousel/@480p__larinha.jpg',
+    galery: [
+      'https://images.pexels.com/photos/3250454/pexels-photo-3250454.jpeg?auto=compress&cs=tinysrgb&w=600',
+      'https://images.pexels.com/photos/2226006/pexels-photo-2226006.jpeg?auto=compress&cs=tinysrgb&w=600',
+      'https://images.pexels.com/photos/1438163/pexels-photo-1438163.jpeg?auto=compress&cs=tinysrgb&w=600',
+    ],
+    vaccins: []
   },
 ]
