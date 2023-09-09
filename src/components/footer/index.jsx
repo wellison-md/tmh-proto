98689089ce0f320f8e8df2067b4ba5462c0ee949
Wellison M. Dias <wellisonmdias@gmail.com>
2023-09-08 22:47:07 -0300

    :zap: feat: create footer-component

---
+++ src/components/footer/index.jsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <>
+      <h4>Take me home - (c) copyright 2023</h4>
+      <p>Termos de uso</p>
+      <p>Política de privacidade</p>
+    </>
+  );
+}
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -3,6 +3,9 @@ import { getFake } from "../../utils/fkdb/getFkData";
 import NavBar from "../../components/navBar";
 import CardList from "../../components/cardList";
 import DonateWidget from "../../components/donateWidget";
+import EventList from "../../components/eventList";
+import { EVENT_LIST } from "../../utils/configs";
+import Footer from "../../components/footer";
 
 export default function Home() {
   const [data, setData] = useState([]);
@@ -20,6 +23,8 @@ export default function Home() {
       <NavBar />
       <CardList petList={ data } />
       <DonateWidget />
+      <EventList eventList={ EVENT_LIST } />
+      <Footer />
     </>
   );
 }
