e739e735532f9b49c2853c9c33bdcf15a18c7ec4
Wellison M. Dias <wellisonmdias@gmail.com>
2023-09-09 17:11:22 -0300

    :zap: feat: create createUser-function

--- src/pages/signup/index.jsx
+++ src/pages/signup/index.jsx
@@ -1,14 +1,40 @@
 import { useState } from "react";
 import NavBar from "../../components/navBar";
+import { getFromStorage, saveOnStorage } from "../../utils/localStorage";
+import { getByProps } from "../../utils/fkdb/getFkData";
+import { USER_TEMPLATE } from '../../utils/configs';
+import { hash } from "bcryptjs";
 
 export default function SignUp() {
   const [usrname, setUsrname] = useState('');
   const [email, setEmail] = useState('');
   const [pswd, setPswd] = useState('');
+  const [status, setStatus] = useState('');
 
   const isActive = usrname.length >= 3
-    && email.length > 6
+    && email.length >= 6
     && pswd.length >= 6
+
+  const createUser = async (email, pswd, usrname) => {
+    const users = getFromStorage('tmh-users');
+    const emailAlreadyExist = getByProps(users, 'email', email);
+
+    if (emailAlreadyExist) {
+      setStatus('Este email não está disponível');
+    }
+
+    const newUser = {
+      ...USER_TEMPLATE,
+      id: users.length + 1,
+      email,
+      usrname,
+      pswd: await hash(pswd, 10),
+    };
+
+    users.push(newUser);
+    saveOnStorage('tmh-users', users);
+    setStatus('');
+  };
 
   return (
     <>
@@ -42,9 +68,14 @@ export default function SignUp() {
         <button
           type='button'
           disabled={ !isActive }
+          onClick={ () => createUser(email, pswd, usrname)}
         >
           Cadastrar
         </button>
+
+        {
+          status
+        }
       </form>
     </>
   );
