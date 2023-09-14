c581b0192fb8a07cf6221bb04355dd0c87def47a
Wellison M. Dias <wellisonmdias@gmail.com>
2023-09-14 17:10:40 -0300

    :zap: style: upgrade Card style

---
+++ src/components/card/cardStyle.jsx
@@ -0,0 +1,33 @@
+import styled from "styled-components";
+
+export const CardWrapper = styled.div`
+  align-items: center;
+  background-color: white;
+  border-radius: 12px;
+  box-shadow: 0 2px 5px #888;
+  cursor: pointer;
+  display: flex;
+  flex-direction: column;
+  height: 500px;
+  margin: 12px 0;
+  max-width: 250px;
+  padding: 8px;
+  transition: .3s;
+  width: 250px;
+
+  & img {
+    height: 100%;
+    object-fit: cover;
+    width: 100%;
+  }
+
+  & h3 {
+    color: #8500d3;
+    font-size: 24px;
+    padding-top: 8px;
+  }
+
+  & p {
+    font-size: 13px;
+  }
+`;
--- src/components/card/index.jsx
+++ src/components/card/index.jsx
@@ -1,21 +1,20 @@
 import PropTypes from 'prop-types';
 import { calculatePetcoins } from '../../utils/petCoins';
 import { formatDateToView } from '../../utils/formaters';
-import { Link } from 'react-router-dom';
+import { CardWrapper } from './cardStyle';
+import { useNavigate } from 'react-router-dom';
 
 export default function PetCard(props) {
   const { id, image, nick, rescue_date, } = props;
+  const navigate = useNavigate();
+
   return (
-    <>
-      <Link to={ `/pet-profile/${id}` } >
-        <img src={ image } alt={ nick } />
-        <h3>{ nick }</h3>
-        <span>
-          <p>Resgatado em: { formatDateToView(rescue_date) }</p>
-          <p>PetCoins: { calculatePetcoins(rescue_date) }</p>
-        </span>
-      </Link>
-    </>
+    <CardWrapper onClick={ () => navigate(`/pet-profile/${id}`) }>
+      <img src={ image } alt={ nick } />
+      <h3>{ nick }</h3>
+      <p>Resgatado em: { formatDateToView(rescue_date) }</p>
+      <p>PetCoins: { calculatePetcoins(rescue_date) }</p>
+    </CardWrapper>
   );
 }
 
