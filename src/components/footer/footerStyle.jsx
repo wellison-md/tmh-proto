5fa471b5575c7a6a15060e4488974f761b1e7f0a
Wellison M. Dias <wellisonmdias@gmail.com>
2023-09-14 15:59:09 -0300

    :zap: style: upgrade footer style

---
+++ src/components/footer/footerStyle.jsx
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+
+export const Bg = styled.div`
+  align-items: center;
+  background-color: #111;
+  color: #999;
+  display: flex;
+  flex-direction: column;
+  padding: 36px;
+`;
+
+export const FooterLabel = styled.h4`
+  color: white;
+  font-size: 20px;
+  padding-bottom: 12px;
+  padding-top: 12px;
+`;
+
+export const FooterIcon = styled.img`
+  height: 36px;
+  max-height: 36px;
+  padding-right: 12px;
+`;
+
+export const FooterHeadline = styled.div`
+  display: flex;
+`;
