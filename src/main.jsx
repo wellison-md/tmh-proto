4e76be8782efd9b41d1b24de48bfad77bfe18e71
wellison m. dias <wellisonmdias@gmail.com>
2023-09-16 10:02:44 -0300

    :zap: feat: create global context

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { GlobalStyles } from './GlobalStyles.jsx'
+import StoreProvider from './context/storeProvider.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <GlobalStyles />
-      <App />
+      <StoreProvider>
+        <App />
+      </StoreProvider>
   </React.StrictMode>,
 )
