39bdc75aa83e5873c8332587a9695ed3f1956b84
wellison m. dias <wellisonmdias@gmail.com>
2023-09-21 00:49:17 -0300

    :zap: feat: create theme provider

---
+++ src/Theme.jsx
@@ -0,0 +1,27 @@
+const theme = {
+  sizes: {
+    xxs: '4px',
+    xs: '8px',
+    s: '12px',
+    xm: '24px',
+    m: '36px',
+    l: '40px',
+    xl: '45px',
+    xxl: '48px',
+    xxxl: '56px',
+  },
+  colors: {
+    primary: '#8500d3',
+    comp1: '#D42F77',
+    comp2: '#EB673F',
+    comp3: '#F6D736',
+  },
+  screens: {
+    xs: '576px',
+    sm: '768px',
+    md: '960px',
+    lg: '1200px',
+  }
+}
+
+export default theme;
--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import { GlobalStyles } from './GlobalStyles.jsx'
+import { ThemeProvider } from 'styled-components'
+import App from './App.jsx'
 import StoreProvider from './context/storeProvider.jsx'
+import theme from './Theme.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <GlobalStyles />
-      <StoreProvider>
-        <App />
-      </StoreProvider>
+      <ThemeProvider theme={theme} >
+        <StoreProvider>
+          <App />
+        </StoreProvider>
+
+      </ThemeProvider>
   </React.StrictMode>,
 )
