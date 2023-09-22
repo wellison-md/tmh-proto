85d64ff48028c59083ce8a28d36146527c4b5326
Wellison M. Dias <wellisonmdias@gmail.com>
2023-09-22 19:27:40 -0300

    :zap: feat: create spinner

---
+++ src/components/spinner/spinnerStyle.jsx
@@ -0,0 +1,22 @@
+import styled, { keyframes } from "styled-components";
+
+const rotateIcon = keyframes`
+  0% { transform: rotate(0deg) }
+  100% { transform: rotate(360deg) }
+`;
+
+export const Image = styled.img`
+  animation: ${rotateIcon} 1s infinite linear;
+  max-width: 100px;
+  padding: ${ ({ theme }) => theme.sizes.xxxl } 0;
+  width: 40px;
+`;
+
+export const SpinnerWrapper = styled.div`
+  align-items: center;
+  display: flex;
+`;
+
+export const Label = styled.p`
+  padding-left: ${ ({ theme }) => theme.sizes.s };
+`;
