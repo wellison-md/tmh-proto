c7bdf066abd230f71c4a3382359b78cd8a10ddd7
wellison m. dias <wellisonmdias@gmail.com>
2023-09-22 23:34:44 -0300

    :recycle: refact: change to spinner comp

--- src/components/eventList/index.jsx
+++ src/components/eventList/index.jsx
@@ -1,5 +1,6 @@
 import PropTypes from 'prop-types';
 import { EventWrapper, Image } from './eventListStyle';
+import Spinner from '../spinner';
 
 export default function EventList(props) {
   const { eventList } = props;
@@ -7,7 +8,7 @@ export default function EventList(props) {
     <EventWrapper>
       {
         eventList.length === 0
-          ? 'loading...'
+          ? <Spinner msg='Carregando...' />
           : eventList.map((evt, i) => (<Image key={ i } src={ evt } alt={ `evento-${i}` } />))
       }
     </EventWrapper>
