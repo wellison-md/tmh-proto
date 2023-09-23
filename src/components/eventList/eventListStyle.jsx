8294d9238f72e8cdeee1d52ee109c84ae1a82092
wellison m. dias <wellisonmdias@gmail.com>
2023-09-22 23:05:07 -0300

    :recycle: refact: update event style

--- src/components/eventList/eventListStyle.jsx
+++ src/components/eventList/eventListStyle.jsx
@@ -1,12 +1,16 @@
 import styled from "styled-components";
 
 export const EventWrapper = styled.div`
+  align-items: center;
   display: flex;
   flex-wrap: wrap;
+  justify-content: center;
+  padding: ${ ({ theme }) => theme.sizes.xxm } 0;
 `;
 
 export const Image = styled.img`
+  border-radius: ${ ({ theme }) => theme.sizes.m };
   max-width: 40%;
-  width: 40%;
-  padding: 2%;
+  padding: 1%;
+  width: 45%;
 `;
