8d4cf5f8f2b58bf82bb934438df9b2d3fd4756a7
wellison m. dias <wellisonmdias@gmail.com>
2023-09-24 17:27:18 -0300

    :bug: fix:  error when refresh on account-page

--- src/components/navBar/index.jsx
+++ src/components/navBar/index.jsx
@@ -1,18 +1,15 @@
 import { Link } from 'react-router-dom';
 import { ListItem, NavBarWrapper } from './navBarStyle';
-import { useContext, useEffect, useState } from 'react';
+import { useContext, useEffect } from 'react';
 import { FaUserCircle } from 'react-icons/fa';
-import { checkLoggedUser, getFromStorage } from '../../utils/localStorage';
+import { checkLoggedUser } from '../../utils/localStorage';
 import Store from '../../context/store';
 
 export default function NavBar() {
   const { loggedUser, setLoggedUser } = useContext(Store);
-  const [currentUser, setCurrentUser] = useState({});
 
   useEffect(() => {
-    setLoggedUser(checkLoggedUser())
-    const usrname = getFromStorage('tmh-logged-user');
-    setCurrentUser(usrname);
+    setLoggedUser(checkLoggedUser());
   }, [setLoggedUser])
 
   return (
@@ -22,7 +19,7 @@ export default function NavBar() {
       <ListItem><Link to='/contact' >Contato</Link></ListItem>
       {
         loggedUser
-          ? <ListItem><FaUserCircle /><Link to={ `/user-profile` } >{ currentUser.usrname || 'conta' }</Link></ListItem>
+          ? <ListItem><FaUserCircle /><Link to={ '/user-profile' } > Conta</Link></ListItem>
           : <ListItem><Link to='/login' >Login</Link></ListItem>
       }
     </NavBarWrapper>
