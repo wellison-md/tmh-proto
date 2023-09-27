2a297fc00798359ccf1776d4c5892d7508673c02
wellison m. dias <wellisonmdias@gmail.com>
2023-09-26 22:52:44 -0300

    :zap: feat: add date-filter component to pet-page

--- src/pages/pets/index.jsx
+++ src/pages/pets/index.jsx
@@ -1,9 +1,13 @@
+import DateFilter from '../../components/filters/dateFilter';
+import Footer from '../../components/footer';
 import Header from '../../components/header';
 
 export default function Pets() {
   return (
     <>
       <Header />
+      <DateFilter />
+      <Footer />
     </>
   );
 }
