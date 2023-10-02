2ab59fd5425cbe7d8638927a50a00257bcb79002
wellison m. dias <wellisonmdias@gmail.com>
2023-10-01 22:15:44 -0300

    :zap: feat: add text label

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -2,11 +2,14 @@ import { useEffect, useState } from 'react';
 import { getFake } from '../../utils/fkdb/getFkData';
 import { checkInitialStorage } from '../../utils/localStorage';
 import { users } from '../../utils/fkdb/fkUsers';
+import { BiSolidZap } from 'react-icons/bi';
 import CardList from '../../components/cardList';
 import DonateWidget from '../../components/donateWidget';
 import EventList from '../../components/eventList';
 import Footer from '../../components/footer';
 import Header from '../../components/header';
+import Flex from '../../components/common/flex';
+import Spacer from '../../components/common/spacer';
 
 checkInitialStorage('tmh-users', users);
 
@@ -27,6 +30,11 @@ export default function Home() {
   return (
     <>
       <Header />
+      <Spacer />
+      <Flex>
+        <h3><BiSolidZap /> Resgatados recentemente</h3>
+      </Flex>
+      <Spacer />
       <CardList petList={ pets } />
       <DonateWidget />
       <EventList eventList={ events } />
